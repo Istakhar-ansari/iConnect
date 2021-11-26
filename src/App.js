537a738cb7510b3e0a6e1d5efa2f89e8a2c9fdf9
MD ISTAKHAR ANSARI <istakhar.ansari@leadschool.in>
2021-11-26 16:09:33 +0530

    added front page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
+import Heading from './components/Heading';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-      <Button color="primary" variant="contained">
-        Click Me
-      </Button>
-      <Button color="primary" variant="contained">
-        Click Me
-      </Button>
-    </div>
-      </header>
-      
-    </div>
-  );
+  return <>
+  <Heading/>
+  </>
 }
 
 export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import './Heading.css'
+import Button from '@mui/material/Button';
+function Heading(){
+    return <div className="container">
+    <button class="button-36" role="button">View</button>
+    <button class="button-37" role="button">Upload</button>
+    <button class="button-38" role="button">Feedback</button>
+    
+    </div>
+}
+export default Heading
