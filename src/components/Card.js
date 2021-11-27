e91ca8efa29407ed332422f9153715e6f97dd46c
MD ISTAKHAR ANSARI <istakhar.ansari@leadschool.in>
2021-11-27 11:09:18 +0530

    feedback section done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import React  from "react";
 import './Card.css'
-function Card(){
+function Card(props){
+    const {item}=props
+    console.log("item aya",item._id);
     return <>
     <div className="cardbox">
-    <h3>something will be here
-    </h3>
-    <h4>Status:<span>pending</span></h4>
-    <h4>Status:<span>pending</span></h4>
-    <h4>Status:<span>pending</span></h4>
-    <h4>Status:<span>pending</span></h4>
-    
+    <h2>{`Class : ${item.classid}   Subject : ${item.subjectid}`}</h2>
+    <h3>{`Feedback : ${item.feedbackText?item.feedbackText:''} `}</h3>
+    {item.comments?<h3>{`Comments : ${item.comments}`}</h3>:null}
+    <h3>{`Status : ${item.status}`}</h3>
+    <h3>{`User Name : ${item.teacher.username} Sir`}</h3>
     </div>
     </>
 }
